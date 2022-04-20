f181ca8f2c9ad0799a55b04ec8531cb978e904c0
ZZIDZZ <zidanutomo@gmail.com>
2022-04-20 18:33:23 +0700

    added tamuctf (no time to complete) and other htb machine

---
+++ HTB/misc_m0rsarchive/zipcentipn_2v2.py
@@ -0,0 +1,48 @@
+from pwn import *
+import glob,os
+from PIL import Image
+
+# fileName = glob.glob(f"*.zip")[0].lstrip(f"./")
+dirTree = ""
+fileIt = 999
+io = process('sh')
+def listDir(keyword = "",dir = "."):
+    arr = []
+    for f in glob.glob(f"{dir}/{keyword}"):
+        arr.append(f.lstrip(f"{dir}/"))
+    return arr
+
+while True:
+    io = process('sh')
+    fileName = f"flag_{fileIt}.zip"
+    print(f"{dirTree}{fileName}")
+    im = Image.open(fileName)
+    width,height = im.size
+    im_rgb = im.convert('RGB')
+    pix = im_rgb.load()
+    morseFromImg = ""
+    morse = ""
+    bgColor = pix[0,0]
+    count = 0
+    for i in range(1,height-1,2):
+        for j in range(1,width-1):
+            print(pix[j,i])
+            if bgColor!=pix[j,i]:
+                count += 1
+            else:
+                if count==1:
+                    morse+="."
+                elif count == 3:
+                    morse+="-"
+                count = 0
+    morseArr = morse.split(" ")
+    print(morseArr)
+    password = io.recvrepeat(timeout=15).decode().strip()[26:]
+    print(password, fileName, dirTree)
+    if "flag" in dirTree:
+        io.sendline(f"unzip -o -qq -P {password} {dirTree}{fileName} -d {dirTree}")
+    else:
+        io.sendline(f"unzip -o -qq -P {password} {dirTree}{fileName}")
+    print(io.recvrepeat(1).decode())
+    dirTree+="flag/"
+    fileIt-=1
